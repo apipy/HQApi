9861acb6e06af5748b1144ab635ebb1a48016a12
Lapki <lapocki@tuta.io>
2018-09-29 16:14:19 +0300

    Login and register (1.0.2)

---
+++ examples/register_login.py
@@ -0,0 +1,17 @@
+from HQApi import HQApi
+
+api = HQApi("")
+
+phone = int(input("Phone... "))
+verification = api.send_code("+" + str(phone), "sms")
+# To request call, send sms, wait 30 seconds and then request call
+code = int(input("Code... "))
+
+api.confirm_code(verification["verificationId"], str(code))
+
+name = str(input("Name... "))
+refferal = str(input("Refferal... "))
+
+bearer = api.login(verification["verificationId"], name, refferal)
+
+print("Bearer: "+bearer["accessToken"])
