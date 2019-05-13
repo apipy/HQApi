026b1e6296e097eb84505b928bf91f06ac0d4d39
Lapki <lapocki@tuta.io>
2019-05-13 20:20:58 +0300

    CI typo fix

--- tests/decode_token.py
+++ tests/decode_token.py
@@ -1,5 +1,5 @@
 from HQApi import HQApi
 
-api = HQApi(token='uQOOiASmugQh2F09qj39LO1DaWUf7bkSeUn4ofHQJl7dcgzNPzReI1aAHmm0Kgwc')
+api = HQApi(logintoken='uQOOiASmugQh2F09qj39LO1DaWUf7bkSeUn4ofHQJl7dcgzNPzReI1aAHmm0Kgwc')
 
 print(api.decode_token())
