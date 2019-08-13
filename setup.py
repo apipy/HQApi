ea15c89d6c1f07bc22945d587189a24723a09e4f
Lapki <lapocki@tuta.io>
2019-08-13 19:57:46 +0300

    Add offair trivia + store (2.3.0)

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="HQApi",
-    version="2.2.0",
+    version="2.3.0",
     author="Katant",
     author_email="katant.savelev@yandex.ru",
     description="HQ Trivia & Words API",
